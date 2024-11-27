cdbe714eea6ca287be09fdd14c89d417cf80c931
Peu-ian <ian.peu.reis@gmail.com>
2024-11-27 20:19:05 -0300

    Create snake.cpp

---
+++ snake.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int Q1, E1, Q2, E2, Q3, E3;
+  cin >> Q1 >> Q2 >> Q3;
+  cin >> E1 >> E2 >> E3;
+cout << (Q1 - (E1)*3) + (Q2 - (E2)*3) + (Q3 - (E3)*3) << endl;
+}
